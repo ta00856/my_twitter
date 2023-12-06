c91b361fe5deded70610e1b40b3e9f8fed8e9c8c
Taukeer <Taukeersurrey@gmail.com>
2023-12-06 11:21:23 +0000

    signup

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print('hello')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
